7f3cc5b934728a39e7acf5309329caf65807080f
mukul kumar <mukulsrivastava05612@gmail.com>
2023-05-05 14:04:29 +0530

    message

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="dpg-chaanu2k728r8871jlrg-a",
+        host="dpg-chaanu2k728r8871jlrg-a.oregon-postgres.render.com",
         database="todo_k0v3",
         user="root",
         password="m6nDDqw1XsniwCAN7kKKGhqebDcJmRHb")
